659e1a1bcfd55637eb1d1a367218b528d3ffe4dd
John Murray <me@johnmurray.io>
2025-02-17 09:14:29 -0500

    [cli] basic option struct + parser struct

--- src/cli.zig
+++ src/cli.zig
@@ -17,7 +17,140 @@ pub fn readArg(alloc: Allocator) !?ArrayList(u8) {
     return arg;
 }
 
-/// A simple argument reader to
+pub const ArgParser = struct {
+    allocator: Allocator,
+    options: ArrayList(option(undefined)) = undefined,
+
+    // Add a flag to the parser. 'T' is used to determine the type of the flag and there are only certain supported
+    // types. A destination must be provided to store the value of the flag. Strings aren't supported here. Use the
+    // 'addStringFlag' method instead.
+    pub fn addFlag(self: *ArgParser, comptime T: type, short: []const u8, long: []const u8, description: []const u8, destination: ?*T) !void {
+        // _ = self;
+        _ = short;
+        _ = long;
+        _ = description;
+        // _ = destination;
+        var op = comptime {
+            if (@TypeOf(T) == i32) {
+                // do something
+                option(i32){
+                    .allocator = self.allocator,
+                    .receiver = destination.?,
+                };
+            } else {
+                // do something else
+            }
+        };
+        _ = &op;
+    }
+
+    // pub fn addStringFlag
+
+    pub fn parse(self: *ArgParser) !void {
+        var reader = ArgReader.init(self.allocator);
+        defer reader.deinit();
+
+        // TODO: iterate over the reader and parse the arguments
+        try reader.read();
+    }
+
+    pub fn init(allocator: Allocator) ArgParser {
+        var parser = .{ .allocator = allocator };
+        parser.options = ArrayList(option(undefined)).init(allocator);
+        return parser;
+    }
+
+    pub fn deinit(self: *ArgParser) void {
+        self.options.deinit();
+    }
+};
+
+//--------------------------------------------------------------------------------
+// `option` struct
+
+/// `option` is a generic struct for representing potential flag options. It is meant to hold
+/// basic types (ints, bools, floats, strings). It does not work for more complex types (lists,
+/// maps, structs, etc). The purpose is to encapsulate all individual flag operations such as
+/// flag matching, parsing, and generating a description.
+fn option(comptime T: type) type {
+    return struct {
+        allocator: Allocator,
+        receiver: *T = undefined,
+        length: usize = 0,
+
+        // Flag names in the format of "--long-name" or "-s"
+        long_name: ?ArrayList(u8) = null,
+        short_name: ?ArrayList(u8) = null,
+
+        const Self = @This();
+
+        pub fn parse(self: *Self, arg: []const u8) !void {
+            // Use the comptime type for the option to delegate to the correct parsing function
+            comptime var parse_fn: *const fn ([]const u8, *T) anyerror!void = undefined;
+            comptime {
+                if (T == i32) {
+                    parse_fn = parse_i32;
+                } else {
+                    // unsupported type
+                    @compileError("Unsupported option type " ++ @typeName(T));
+                }
+            }
+            try parse_fn(arg, self.receiver);
+        }
+
+        pub fn with_long_name(self: *Self, name: []const u8) !void {
+            self.long_name = ArrayList(u8).init(self.allocator);
+            try self.long_name.?.appendSlice(name);
+        }
+
+        pub fn with_short_name(self: *Self, name: []const u8) !void {
+            self.short_name = ArrayList(u8).init(self.allocator);
+            try self.short_name.?.appendSlice(name);
+        }
+
+        pub fn deinit(self: *Self) void {
+            if (self.long_name) |name| {
+                name.deinit();
+            }
+            if (self.short_name) |name| {
+                name.deinit();
+            }
+        }
+    };
+}
+
+test "i32 option" {
+    const i32_op = option(i32);
+
+    var receiver: i32 = 0;
+    var op = i32_op{ .allocator = t.allocator, .receiver = &receiver };
+    defer op.deinit();
+
+    try op.with_short_name("-u");
+    try op.with_long_name("--boop");
+    try op.parse("64");
+
+    try t.expectEqual(64, receiver);
+}
+
+//--------------------------------------------------------------------------------
+// Parsing functions
+
+fn parse_i32(arg: []const u8, dest: *i32) !void {
+    var value: i32 = 0;
+    for (arg) |c| {
+        if (c < '0' or c > '9') {
+            return error.InvalidInteger;
+        }
+        value = value * 10 + (c - '0');
+    }
+    dest.* = value;
+}
+
+//--------------------------------------------------------------------------------
+// Input Argument Reader / Iterator
+
+/// A simple argument reader to iterate over argument passed to a program.
 pub const ArgReader = struct {
     allocator: Allocator,
     args: ?[][:0]u8 = null,
